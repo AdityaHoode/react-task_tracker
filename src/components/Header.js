7cb4f34546c10851b512183d0a440ca90e27d565
adihoo <adityahoode3@gmail.com>
2021-04-27 00:17:51 +0530

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = (props) => {
   const [toggleBtnTxt, setToggleBtnTxt] = useState(true);
+  const location = useLocation();
   return (
     <div className="header">
       <h1>{props.title}</h1>
-      <Button
-        color={toggleBtnTxt ? "green" : "red"}
-        text={toggleBtnTxt ? "Add" : "Close"}
-        onClick={() => {
-          props.onAdd();
-          setToggleBtnTxt(!toggleBtnTxt);
-        }}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={toggleBtnTxt ? "green" : "red"}
+          text={toggleBtnTxt ? "Add" : "Close"}
+          onClick={() => {
+            props.onAdd();
+            setToggleBtnTxt(!toggleBtnTxt);
+          }}
+        />
+      )}
     </div>
   );
 };
